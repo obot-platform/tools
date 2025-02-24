4df3147167907ce81d02d748bf8ebb0f8c0aa772
Taylor Price <tayworm@gmail.com>
2025-02-24 11:04:11 -0700

    chore: s3 tools
    Signed-off-by: Taylor Price <tayworm@gmail.com>

---
+++ aws/s3/pkg/commands/helper.go
@@ -0,0 +1,44 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	smithyhttp "github.com/aws/smithy-go/transport/http"
+)
+
+func NewRegionMatchedS3Client(ctx context.Context, client *s3.Client, bucket string) (*s3.Client, error) {
+	var err error
+	var region string
+	_, err = client.HeadBucket(ctx, &s3.HeadBucketInput{
+		Bucket: aws.String(bucket),
+	})
+	if err == nil {
+		region = client.Options().Region
+		return client, nil
+	}
+
+	var respErr *smithyhttp.ResponseError
+	if errors.As(err, &respErr) {
+		hdr := respErr.Response.Header.Get("x-amz-bucket-region")
+		if hdr != "" {
+			region = hdr
+		}
+	}
+
+	if region == "" {
+		return nil, fmt.Errorf("failed to find s3 region: %w", err)
+	}
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load AWS config: %v", err)
+	}
+
+	cfg.Region = region
+	regionalClient := s3.NewFromConfig(cfg)
+
+	return regionalClient, nil
+}
