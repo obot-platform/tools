5bce583333ad54d75c827a9ae166afe9ca324b7e
Grant Linville <grant@acorn.io>
2025-03-03 16:30:29 -0500

    add validate tool
    Signed-off-by: Grant Linville <grant@acorn.io>

--- postgres/main.go
+++ postgres/main.go
@@ -31,6 +31,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Check for validate mode
+	if len(os.Args) > 1 && os.Args[1] == "validate" {
+		// Test the connection with a simple query
+		var result int
+		err = db.Raw("SELECT 1").Scan(&result).Error
+		if err != nil {
+			fmt.Printf("Error validating connection: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Database connection validated successfully")
+		os.Exit(0)
+	}
+
 	// Check if the query returns data
 	queryUpper := strings.TrimSpace(strings.ToUpper(query))
 	returnsData := strings.Contains(queryUpper, "SELECT") ||
