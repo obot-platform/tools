4389d93a6c0a6b6c8635913a65eeaafbd0d036a8
Daishan Peng <daishan@acorn.io>
2025-11-04 09:36:46 -0700

    Fix: remove used notion and knowledge
    Signed-off-by: Daishan Peng <daishan@acorn.io>

--- discord/src/types/gptscript.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@gptscript-ai/gptscript' {
-  export class GPTScript {
-    addDatasetElements(elements: Array<{
-      name: string;
-      description: string;
-      contents: string;
-    }>, options: { name: string }): Promise<string>;
-  }
-} 
